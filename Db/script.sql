88264e299f11f84ace61783d5e38da30d79249c6
Paul Veliz <paulsystem32@gmail.com>
2021-04-21 11:22:19 -0600

    Db data

--- Db/script.sql
+++ Db/script.sql
@@ -46,3 +46,41 @@ CREATE TABLE Solicitudes(
 ----------------------- FIXES ---------------------------------
 ALTER TABLE Clientes ADD id_estado_civil int
 ALTER TABLE Clientes ADD FOREIGN KEY (id_estado_civil) REFERENCES Estados_civiles(Id);
+
+ALTER TABLE Clientes
+DROP COLUMN Fecha_nacimiento;
+
+
+------------- DATA ---------------
+INSERT INTO Estados_civiles(tipo)VALUES('Casado')
+INSERT INTO Estados_civiles(tipo)VALUES('Soltero')
+
+INSERT INTO Clientes(Nombre_completo, Domicilio, Curp, Ingresos_mensuales, Url_imagen, Edad, Hijos, id_estado_civil)
+VALUES('MARTINA ALTAMIRANO CALDERON', 'SINALOA', 'AACM651123MTSLLR06', 8500, NULL, 28, 2, 1)
+
+INSERT INTO Clientes(Nombre_completo, Domicilio, Curp, Ingresos_mensuales, Url_imagen, Edad, Hijos, id_estado_civil)
+VALUES('ZULEIMA GABRIELA ORDOÑES ALANIS', 'SINALOA', 'OOAZ900824MTSRLL08', 13400, NULL, 22, 0, 2)
+
+INSERT INTO Clientes(Nombre_completo, Domicilio, Curp, Ingresos_mensuales, Url_imagen, Edad, Hijos, id_estado_civil)
+VALUES('MARISOL SANCHEZ PEREZ', 'SINALOA', 'SAPM880429MTSNRR00', 10200, NULL, 32, 0, 1)
+
+INSERT INTO Clientes(Nombre_completo, Domicilio, Curp, Ingresos_mensuales, Url_imagen, Edad, Hijos, id_estado_civil)
+VALUES('MAGDALENA GONZALEZ RODRIGUEZ', 'SINALOA', 'GORM680121MTSNDG06', 8500, NULL, 29, 3, 1)
+
+INSERT INTO Clientes(Nombre_completo, Domicilio, Curp, Ingresos_mensuales, Url_imagen, Edad, Hijos, id_estado_civil)
+VALUES('GUADALUPE RODRIGUEZ BARTOLO', 'SINALOA', 'ROBG900321MTSDRD01', 8500, NULL, 35, 1, 1)
+
+INSERT INTO Clientes(Nombre_completo, Domicilio, Curp, Ingresos_mensuales, Url_imagen, Edad, Hijos, id_estado_civil)
+VALUES('YULETH ARELY ORTIZ RODRIGUEZ', 'SINALOA', 'OIRY910429MTSRDL02', 8500, NULL, 45, 0, 2)
+
+INSERT INTO Clientes(Nombre_completo, Domicilio, Curp, Ingresos_mensuales, Url_imagen, Edad, Hijos, id_estado_civil)
+VALUES('JUAN ESPINOZA RODRIGUEZ', 'SINALOA', 'EIRJ720502HTSSDN06', 8500, NULL, 23, 0, 2)
+
+INSERT INTO Tarjetas_credito (Descripcion, Limite_credito, Tasa_interes_anual, Min_ingreso_acumulable) 
+VALUES('BASICO', 5000, 65, 4000)
+
+INSERT INTO Tarjetas_credito (Descripcion, Limite_credito, Tasa_interes_anual, Min_ingreso_acumulable) 
+VALUES('ORO', 25000, 55, 4001)
+
+INSERT INTO Tarjetas_credito (Descripcion, Limite_credito, Tasa_interes_anual, Min_ingreso_acumulable) 
+VALUES('PLATINUM', 100000, 45, 15001)
